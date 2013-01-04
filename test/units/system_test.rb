9045c999fbc86d2ce816334d0bb96e50f36eb638
Sam Soffes <sam@soff.es>
2013-01-04 00:01:24 -0500

    Add singleton test This is a crap test. I just want Travis to fire

--- test/units/system_test.rb
+++ test/units/system_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SystemTest < SonosTest
+  def test_singleton
+    assert_equal Sonos.system.object_id, Sonos.system.object_id
+  end
+
   def test_group_detection
     VCR.use_cassette('topology', match_requests_on: [:path]) do
       system = Sonos.system
