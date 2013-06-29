805de30cae5d3d10fcb0abe2f167307a730f070c
Sam Soffes <sam@soff.es>
2013-06-29 13:27:10 -0400

    Add party mode to CLI

--- lib/sonos/cli.rb
+++ lib/sonos/cli.rb
@@ -17,17 +17,27 @@ module Sonos
       end
     end
 
-    desc 'pause_all', 'Pauses all Sonos speaker groups'
+    desc 'pause_all', 'Pause all speaker groups.'
     def pause_all
     	system.pause_all
     end
 
-    desc 'play_all', 'Resumes playing all Sonos speaker groups'
+    desc 'play_all', 'Resume playing all speaker groups.'
     def play_all
       system.play_all
     end
 
-    desc 'groups', 'List all Sonos groups'
+    desc 'party_mode', 'Start a party! Put all speakers in the same group.'
+    def party_mode
+      system.party_mode
+    end
+
+    desc 'party_over', 'No more party :( Put all speakers in their own group.'
+    def party_over
+      system.party_over
+    end
+
+    desc 'groups', 'List all groups'
     def groups
       system.groups.each do |group|
         puts group.master_speaker.name.ljust(20) + group.master_speaker.ip
