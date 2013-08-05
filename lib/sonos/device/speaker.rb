88863eba4ef011e04f640f2b77553503898fd4bf
David Pfeffer <david@pfeffer.org>
2013-08-04 18:31:07 -0700

    added support for shuffle, repeat, and crossfade

--- lib/sonos/device/speaker.rb
+++ lib/sonos/device/speaker.rb
@@ -23,5 +23,66 @@ module Sonos::Device
     def speaker?
       true
     end
+
+    def shuffle_on
+      shuffle_repeat_change("shuffle_on")
+    end
+
+    def shuffle_off
+      shuffle_repeat_change("shuffle_off")
+    end
+
+    def repeat_on
+      shuffle_repeat_change("repeat_on")
+    end
+
+    def repeat_off
+      shuffle_repeat_change("repeat_off")
+    end
+
+    def crossfade_on
+      set_crossfade(true)
+    end
+
+    def crossfade_off
+      set_crossfade(false)
+    end
+    
+    def shuffle_repeat_change(command)
+      status = get_playmode
+      case command
+      when "shuffle_on"
+        status[:shuffle] = true;
+      when "shuffle_off"
+        status[:shuffle] = false;
+      when "repeat_on"
+        status[:repeat] = true;
+      when "repeat_off"
+        status[:repeat] = false;
+      end
+      set_playmode(status)
+    end
+
+    def set_playmode(status = {:shuffle => false, :repeat => false})
+      send_transport_message('SetPlayMode', "<NewPlayMode>SHUFFLE</NewPlayMode>")           if (status[:shuffle]  && status[:repeat] )
+      send_transport_message('SetPlayMode', "<NewPlayMode>SHUFFLE_NOREPEAT</NewPlayMode>")  if (status[:shuffle]  && !status[:repeat])
+      send_transport_message('SetPlayMode', "<NewPlayMode>REPEAT_ALL</NewPlayMode>")        if (!status[:shuffle] && status[:repeat] )
+      send_transport_message('SetPlayMode', "<NewPlayMode>SHUFFLE_NOREPEAT</NewPlayMode>")  if (!status[:shuffle] && !status[:repeat])
+    end
+
+    def set_crossfade(crossfade)
+      crossfade_value = crossfade ? 1 : 0
+      send_transport_message('SetCrossfadeMode', "<InstanceID>0</InstanceID><CrossfadeMode>#{crossfade_value}</CrossfadeMode>")
+    end
+
+    def get_playmode
+      doc = Nokogiri::XML(open("http://#{self.group_master.ip}:#{Sonos::PORT}/status/playmode"))
+      playmode = {}
+      playmode[:shuffle] = doc.xpath('//Shuffle').inner_text == "On" ? true : false
+      playmode[:repeat] = doc.xpath('//Repeat').inner_text == "On" ? true : false
+      playmode[:crossfade] = doc.xpath('//Crossfade').inner_text == "On" ? true : false
+      playmode
+    end
+    
   end
 end
