7711630d548e651694acccb62cf1d61288795464
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-01-20 21:52:54 +0100

    Add a method to check the status led status

--- lib/sonos/endpoint/device.rb
+++ lib/sonos/endpoint/device.rb
@@ -2,6 +2,14 @@ module Sonos::Endpoint::Device
   DEVICE_ENDPOINT = '/DeviceProperties/Control'
   DEVICE_XMLNS = 'urn:schemas-upnp-org:service:DeviceProperties:1'
 
+  # Retrieve the status light state; true if on, false otherwise.
+  def status_light_enabled?
+    response = send_device_message('GetLEDState', '')
+    body = response.body[:get_led_state_response]
+
+    body[:current_led_state] == 'On' ? true : false
+  end
+
   # Turn the white status light on or off
   # @param [Boolean] True to turn on the light. False to turn off the light.
   def status_light_enabled=(enabled)
