52ea5f439b4cde8ee26f5e5c1c6c9e9f1b869271
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-05-28 15:36:59 +0200

    Fixes metadata did not correctly show up in Sonos

--- lib/sonos/endpoint/a_v_transport.rb
+++ lib/sonos/endpoint/a_v_transport.rb
@@ -145,7 +145,7 @@ module Sonos::Endpoint::AVTransport
     # In order for the player to retrieve track duration, artist, album etc
     # we need to pass it some metadata ourselves.
     didl_metadata = <<DIDL
-    lt;DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"&gt;&lt;item id="#{rand(10000000..19999999)}spotify%3a#{opts[:type]}%3a#{opts[:id]}" restricted="true"&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.item.audioItem.musicTrack&lt;/upnp:class&gt;&lt;desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/"&gt;SA_RINCON2311_X_#Svc2311-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;
+    &lt;DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"&gt;&lt;item id="#{rand(10000000..19999999)}spotify%3a#{opts[:type]}%3a#{opts[:id]}" restricted="true"&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.item.audioItem.musicTrack&lt;/upnp:class&gt;&lt;desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/"&gt;SA_RINCON2311_X_#Svc2311-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;
 DIDL
 
     r_id = rand(10000000..19999999)
